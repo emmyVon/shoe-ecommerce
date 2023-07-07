69228db31cf28dff77cf9f0195277a6f67fdf56e
EMMANUEL OBIEZUO <emmanuelobiezuo@gmail.com>
2023-07-08 00:51:36 +0800

    deploy

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,27 +1,39 @@
-import React, { useState } from 'react';
-import pro1 from './images/image-product-1.jpg'
-import deleteIcon from './images/icon-delete.svg';
+import React from "react";
+import pro1 from "./images/image-product-1.jpg";
+import deleteIcon from "./images/icon-delete.svg";
 
-const Cart = ({itemNo,sum,price,cart,setCart}) => {
- 
+const Cart = ({ itemNo, sum, price, cart, setCart }) => {
   return (
-    <div className='cart'>
-        <h2>Cart</h2>
-        {cart.empty || itemNo===0?<p>Your cart is empty</p>: 
-        cart.Added && <div className='cart-info'>
+    <div className="cart">
+      <h2>Cart</h2>
+      {cart.empty || itemNo === 0 ? (
+        <p>Your cart is empty</p>
+      ) : (
+        cart.Added && (
+          <div className="cart-info">
             <div>
-                <img src={pro1} alt='pro1'/>
-                <div>
-                    <p>Fall Limited Edition Sneakers</p>
-                    <p>${price} x {itemNo}  <div style={{display:'inline-block', fontWeight:'bold'}}>{sum(itemNo)}</div></p>
-                </div>
-                <img src={deleteIcon} onClick={()=>setCart({...cart, empty:true})} alt='delete'/>
+              <img src={pro1} alt="pro1" />
+              <div>
+                <p>Fall Limited Edition Sneakers</p>
+                <p>
+                  ${price} x {itemNo}{" "}
+                  <div style={{ display: "inline-block", fontWeight: "bold" }}>
+                    {sum(itemNo)}
+                  </div>
+                </p>
+              </div>
+              <img
+                src={deleteIcon}
+                onClick={() => setCart({ ...cart, empty: true })}
+                alt="delete"
+              />
             </div>
-         <button>checkout</button> 
-        </div>
-         }   
+            <button>checkout</button>
+          </div>
+        )
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
